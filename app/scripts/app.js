1cd24741e150b453d17bbc62c095321529e44f8c
Ismail Liban <iliban@jcvi.org>
2015-07-16 11:01:13 -0400

    Returns predicted data as well.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -14,11 +14,12 @@
       data = data.result;
 
       // Creates a base table that the data will be stored in
-      $('#experimental-table', appContext).html(
-        '<table class="table table-striped table-bordered .table-hover"' +
-        'cellspacing="0" width="100%"><thead><tr><th>Peptide Sequence</th>' +
-        '<th>Position</th><th>Modification Type</th><th>Mass</th>' +
-        '</tr></thead><tbody id="experimental-data"></tbody></table>');
+      $('#experimental', appContext).html(
+        '<table width="100%" cellspacing="0" id="experimental-table"' +
+        'class="table table-striped table-bordered table-hover">' +
+        '<thead><tr><th>Peptide Sequence</th><th>Position</th>' +
+        '<th>Modification Type</th><th>Mass</th></tr></thead>' +
+        '<tbody id="experimental-data"></tbody></table>');
 
       for (var i = 0; i < data.length; i++) {
         // Saves data in strings to later be added to table
@@ -33,7 +34,46 @@
       }
 
       // Converts normal table to DataTable
-      $('table', appContext).DataTable({
+      $('#experimental-table', appContext).DataTable({
+        // Overrides default text to make it more specific to this App
+        oLanguage: {
+          sSearch: 'Narrow results:',
+          sEmptyTable: 'No phosphorylation data available.'
+        }
+      });
+
+    };
+
+    // Creates a table to display predicted data
+    // Function is called once search button is clicked
+    var showPredictedData = function showPredictedData(response) {
+      // Stores API response
+      var data = response.obj || response;
+      data = data.result;
+
+      // Creates a base table that the data will be stored in
+      $('#predicted', appContext).html(
+        '<table id="predicted-table" width="100%" cellspacing="0"' +
+        'class="table table-striped table-bordered table-hover">' +
+        '<thead><tr><th>Protein Position</th><th>13-mer Sequence</th>' +
+        '<th>Prediction Score</th></tr></thead>' +
+        '<tbody id="predicted-data"></tbody></table>');
+
+      for (var i = 0; i < data.length; i++) {
+        // Saves data in strings to later be added to table
+        var proteinPos = '<td>' + data[i].position_in_protein + '</td>';
+        // TODO: Modify API to rename 13mer_sequence to thirteen_mer_sequence
+        var sequence = '<td>' + data[i].thirteen_mer_sequence + '</td>';
+        var predictionScore = '<td>' + data[i].prediction_score + '</td>';
+
+
+        // Dynamically adds saved data to the table
+        $('#predicted-data', appContext).append('<tr>' + proteinPos +
+        sequence + predictionScore + '</tr>');
+      }
+
+      // Converts normal table to DataTable
+      $('#predicted-table', appContext).DataTable({
         // Overrides default text to make it more specific to this App
         oLanguage: {
           sSearch: 'Narrow results:',
@@ -48,6 +88,7 @@
 
       // Empties out any tables from old searches
       $('#experimental-table', appContext).empty();
+      $('#predicted-table', appContext).empty();
 
       // Saves user-input as a parameter
       var params = {
@@ -60,6 +101,15 @@
          queryParams: params},
         showExperimentalData // Calls showExperimentalData() after click
       );
+
+      // Calls PhosPhAt API to make predicted search, using saved parameter
+      Agave.api.adama.search(
+        {namespace: 'iliban-dev', service: 'phosphorylated_predicted_v0.1',
+         queryParams: params},
+        showPredictedData // Calls showPredictedData() after click
+      );
+
+
     });
   });
 })(window, jQuery);
